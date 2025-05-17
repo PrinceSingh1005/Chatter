875e64b4a0d92270e3fb8e199a3129c5d6c540cc
Prince <12prince345singh@gmail.com>
2025-05-17 21:19:03 +0530

    fixed the bugs

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"], // Your frontend URL
+        origin: ["http://localhost:3000"], // Your frontend URL
         methods: ["GET", "POST"],
         credentials: true
     }
@@ -25,12 +25,12 @@ io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     if (userId != "undefined") {
         userSocketMap[userId] = socket.id;
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        io.emit("onlineUsers",Object.keys(userSocketMap));
     }
     socket.on("disconnect", () => {
         console.log(`User disconnected: ${socket.id}`);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        io.emit("onlineUsers",Object.keys(userSocketMap));
     });
 
     // Add more event handlers as needed
